bf9895d6d93726a4eafc6f94cacfe2c61493edcf
larissaolimpio <lrsolimpio@gmail.com>
2024-06-07 16:51:02 -0300

    feat: add UserController, UserRepository, and UserService classes
    - The UserController class was added to handle HTTP requests related to users.
    - The UserRepository class was added to interact with the database and perform CRUD operations on users.
    - The UserService class was added to handle business logic related to users.

---
+++ src/main/java/alura/com/br/api/controller/UserController.java
@@ -0,0 +1,35 @@
+package alura.com.br.api.controller;
+import alura.com.br.api.domain.users.Users;
+import alura.com.br.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Users> createUser(@RequestBody Users user) {
+        Users createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<Users> getUserByUsername(@PathVariable String username) {
+        Optional<Users> user = userService.getUserByUsername(username);
+        return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
+
